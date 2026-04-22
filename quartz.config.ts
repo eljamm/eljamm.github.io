dc2edbbe43e784923e4ce02790886a2b71296224
eljamm <fedi.jamoussi@protonmail.ch>
2026-04-22 22:10:39 +0200

    feat: update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "eljamm",
+    pageTitle: "Fedi{eljamm};",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
