523d21ecf759231b74f1dcdc9711ca08c85788f6
eljamm <fedi.jamoussi@protonmail.ch>
2026-05-01 19:32:31 +0200

    fix: ignore neovim conform temporary files when building

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "eljamm.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**/.conform*"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
